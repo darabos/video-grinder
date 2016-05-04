494b94ee347f7e1a389337565507e0c6cbd06de9
Daniel Darabos <darabos.daniel@gmail.com>
2016-05-04 10:34:48 +0200

    Choose files, highlight selection.

--- editor.js
+++ editor.js
@@ -1,86 +1,111 @@
+'use strict';
+
+var selected;
+var data = [];
+
+function addVideos(videos) {
+  var outside = $('#videos');
+  var existing = data.length;
+  for (var i = 0; i < videos.length; ++i) {
+    var file = videos[i];
+    var url = window.URL.createObjectURL(file);
+    var inside = $('<div class="video"></div>');
+    var video = $('<video preload="auto" src="' + url + '"></video>');
+    var meta = $('<div class="meta"></div>');
+    var name = $('<span class="name">' + file.name + '</span>');
+    var spans = $('<span class="start">?</span>&ndash;<span class="end">?</span>');
+    meta.append(name);
+    meta.append(spans);
+    inside.append(video);
+    inside.append(meta);
+    outside.append(inside);
+    var v = video[0];
+    v.dataset.index = existing + i;
+    data.push({ file: file.name });
+
+    video.click(function(e) {
+      setSelected(e.target);
+      toggle(selected);
+    });
+
+    video.mousemove(function(e) {
+      setSelected(e.target);
+      var frame = Math.floor(10 * selected.duration * e.offsetX / selected.offsetWidth);
+      selected.currentTime = frame / 10;
+    });
+  }
+}
+
+function index(v) {
+  return parseInt(v.dataset.index);
+}
+
+function d(v) {
+  return data[index(v)];
+}
+
+function toggle(v) {
+  if (v.paused) {
+    v.play();
+    var end = d(v).end;
+    if (end !== undefined) {
+      v.addEventListener('timeupdate', function() {
+        if(this.currentTime >= end) {
+          this.pause();
+        }
+      });
+    }
+  } else {
+    v.pause();
+  }
+}
+
+function timestr(t) {
+  var min = Math.floor(t / 60);
+  var sec = t - 60 * min;
+  if (sec < 10) {
+    return min.toFixed(0) + ':0' + sec.toFixed(0);
+  } else {
+    return min.toFixed(0) + ':' + sec.toFixed(0);
+  }
+}
+
+function setSelected(v) {
+  if (selected !== undefined) {
+    $(selected).parent().removeClass('selected');
+  }
+  selected = v;
+  $(selected).parent().addClass('selected');
+}
+
 $(function() {
-  var vobj = $('video');
-  var v = vobj[0];
-  var data = [];
-
-  vobj.each(function(i, v) {
-    v.dataset.index = i;
-    var file = v.currentSrc.split('/').slice(-1)[0];
-    data.push({ file: file });
-    $(v).after($('<span class="start">?</span>&ndash;<span class="end">?</span>'));
-  });
-
-  vobj.mousemove(function(e) {
-    v = e.target;
-    var frame = Math.floor(10 * v.duration * e.offsetX / v.offsetWidth);
-    v.currentTime = frame / 10;
-  });
-
-  function index(v) {
-    return parseInt(v.dataset.index);
-  }
-
-  function d(v) {
-    return data[index(v)];
-  }
-
-  function toggle(v) {
-    if (v.paused) {
-      v.play();
-      var end = d(v).end;
-      if (end !== undefined) {
-        v.addEventListener('timeupdate', function() {
-          if(this.currentTime >= end) {
-            this.pause();
-          }
-        });
-      }
-    } else {
-      v.pause();
-    }
-  }
-
-  function timestr(t) {
-    var min = Math.floor(t / 60);
-    var sec = t - 60 * min;
-    if (sec < 10) {
-      return min.toFixed(0) + ':0' + sec.toFixed(0);
-    } else {
-      return min.toFixed(0) + ':' + sec.toFixed(0);
-    }
-  }
-
-  vobj.click(function(e) {
-    v = e.target;
-    toggle(v);
-  });
-
   $('body').keydown(function(e) {
     var k = e.which;
     if (k == 39) { // Right.
-      v.currentTime += 0.1;
+      selected.currentTime += 0.1;
     } else if (k == 37) { // Left.
-      v.currentTime -= 0.1;
+      selected.currentTime -= 0.1;
     } else if (k == 40) { // Down.
-      v.currentTime = 0;
+      selected.currentTime = 0;
     } else if (k == 38) { // Up.
-      v.currentTime = v.duration;
+      selected.currentTime = selected.duration;
     } else if (k == 32) { // Space.
-      toggle(v);
+      toggle(selected);
+      e.preventDefault();
     } else if (k == 13) { // Enter.
-      v.currentTime = d(v).start || 0;
+      selected.currentTime = d(selected).start || 0;
     } else if (k == 'S'.charCodeAt(0)) {
-      d(v).start = v.currentTime;
-      $(v).siblings('.start').html(timestr(v.currentTime));
+      d(selected).start = selected.currentTime;
+      $(selected).siblings('.start').html(timestr(selected.currentTime));
     } else if (k == 'E'.charCodeAt(0)) {
-      d(v).end = v.currentTime;
-      $(v).siblings('.end').html(timestr(v.currentTime));
+      d(selected).end = selected.currentTime;
+      $(selected).siblings('.end').html(timestr(selected.currentTime));
     } else if (k == 'J'.charCodeAt(0)) {
-      var next = vobj[index(v) + 1];
-      if (next !== undefined) { v = next; }
+      var next = $('#videos video')[index(selected) + 1];
+      if (next !== undefined) { setSelected(next); }
     } else if (k == 'K'.charCodeAt(0)) {
-      var prev = vobj[index(v) - 1];
-      if (prev !== undefined) { v = prev; }
+      var prev = $('#videos video')[index(selected) - 1];
+      if (prev !== undefined) { setSelected(prev); }
     }
   });
 
